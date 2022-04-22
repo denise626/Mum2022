fd282ad71a5a07dcf162c3d51a300268a233aaa4
denise626 <denise626@hotmail.com>
2022-04-21 22:14:12 -0400

    finished js code for project

--- js/site.js
+++ js/site.js
@@ -1,36 +1,60 @@
-//Get the string from the page
-//controller function
-function getString(){
+//get our user input
+function getValue(){
 
+    //make sure the alert is invisible
     document.getElementById("alert").classList.add("invisible");
+    //get user string from the page
     let userString = document.getElementById("userString").value;
-
-    let revString = reverseString(userString);
-
-    displayString(revString);
+    //check for a palindrome
+    let returnObj = checkForPalindrome(userString);
+    //display our message to the screen
+    displayMessage(returnObj);
 }
 
-//Reverse the string
-//logic function
-function reverseString(userString){
 
-    let revString = [];
+//check if string is a palindrome
+function checkForPalindrome(userString){
 
-    //reverse a string using a for loop
-    for (let index = userString.length - 1; index >=0; index--) {
+        userString = userString.toLowerCase();
 
-        revString += userString[index];
-        
-    }
-    return revString;
+        //remove spaces and special characters
+        let regex = /[^a-z0-9]/gi;
+        userString = userString.replace(regex,"");
+
+        let revString = [];
+        let returnObj = {};
+
+        for (let index = userString.length - 1; index >= 0; index--) {
+            revString += userString[index];
+            
+        }
+
+        if (revString == userString){
+            returnObj.msg = "Well done! You entered a palindrome!"
+        }
+        else{
+            returnObj.msg = "Sorry! You did not enter a palindrome!"
+        }
+
+        returnObj.reversed = revString;
+
+        return returnObj;
 }
 
-//Display the reversed string to the user
-//view function
-function displayString(revString){
 
-    //write to the page
-    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
-    //show the alert box
+function displayMessage(returnObj){
+
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`
     document.getElementById("alert").classList.remove("invisible");
 }
+
+
+
+
+
+
+
+
+
+
